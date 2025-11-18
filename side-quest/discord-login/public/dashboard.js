60d6d6d7f82bcfa2fecfbb8adea7801a60613461
Octerry <taaniyu@gmail.com>
2025-11-18 21:10:25 +0100

    Debuts des test de connexion à discord

---
+++ side-quest/discord-login/public/dashboard.js
@@ -0,0 +1,35 @@
+// récupération des tokens
+const fragment = new URLSearchParams(window.location.hash.slice(1));
+const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+
+
+// token pour requetes
+let secretsUser = `${tokenType} ${accessToken}`
+
+
+// récupération des infos de base de l'user (pseudo, pp etc...)
+fetch('https://discord.com/api/users/@me', {
+    headers: {
+        authorization: `${secretsUser}`,
+    },
+})
+.then(result => result.json())
+.then(response => {
+    const { username, discriminator, avatar, id} = response;
+
+    //ICI ON PEUT INJECTER DES TRUCS DANS LE HTML EN MODE "getElementById"
+
+
+})
+
+// récupération de la liste des serveurs de l'user
+fetch('https://discord.com/api/users/@me/guilds', {
+    headers: {
+        authorization: `${secretsUser}`,
+    },
+})
+.then(result => result.json())
+.then(responseGuilds => {
+    // ICI ON PEUT UTILISER LES SERVEURS DES GENS
+})
+.catch(console.error)
---
+++ side-quest/discord-login/public/login.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use('', express.static(path.join(__dirname, 'side-quest/discord-login/public')));
+
+app.get('/home', (request, response) => {
+    return response.sendFile('side-quest/discord-login/home.html', { root: '.'});
+});
+
+app.get('/', (request, response) => {
+    return response.sendFile('side-quest/discord-login/index.html', { root: '.'});
+});
+
+const port = '5500'
+app.listen(port, () => console.log(`App listeniong at localhost:${port}`));
