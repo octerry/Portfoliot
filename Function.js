565266cff850a8cb3b7f074d32e772fc618c62a2
Octerry <taaniyu@gmail.com>
2025-11-25 20:16:33 +0100

    Avancement non concluant sur le menu flottant

--- Function.js
+++ Function.js
@@ -1,3 +1,5 @@
+//MENU FLOTTANT
+//OUVERTURE DU MENU FLOTTANT
 function indexHideMenu () {
     var menu = document.getElementById('indexMenu')
     let isHide = (menu.style.marginTop == "0px")
@@ -13,3 +15,18 @@ function indexHideMenu () {
 
     console.log(document.body.classList)
 }
+
+
+//ANIMATION DU FOND DU MENU FLOTTANT
+const indexMenu = document.getElementById("indexMenu")
+
+let indexCode = document.getElementById("indexMenuCode")
+indexCode.addEventListener("mouseenter", () => {
+    indexMenu.style.backgroundImage = 'url("source/illustrationCode.jpeg")'
+    indexMenu.style.backgroundSize = 'cover'
+    indexMenu.style.transition = 'background-image 2s'
+})
+
+indexCode.addEventListener("mouseleave", () => {
+        indexMenu.style.background = '#00000070'
+})
