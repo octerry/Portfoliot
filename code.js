f17338d6123e8e0475083e4a9dffb932e70c5f3b
Octerry <taaniyu@gmail.com>
2026-02-09 23:31:20 +0100

    Debut de la section assistance

--- code.js
+++ code.js
@@ -10,12 +10,6 @@
 
 const codeCursor = document.getElementById('code_up_cursor');
 
-const headerButton = document.getElementsByClassName('header_button');
-const acceuilButton = headerButton[0];
-const codeButton = headerButton[1];
-const robotButton = headerButton[2];
-const plusButton = headerButton[3];
-
 const webButton = document.getElementById('web_button');
 const pythonButton = document.getElementById('python_button');
 const autreButton = document.getElementById('autre_button');
@@ -29,6 +23,18 @@ const preambulePageContent = document.getElementById('preambule_page_content');
 const webPageContent = document.getElementById('web_page_content');
 const pythonPageContent = document.getElementById('python_page_content');
 const autrePageContent = document.getElementById('autre_page_content');
+
+const acceuilButton = document.getElementById('acceuil_button');
+const acceuilMenu = document.getElementById('acceuil_menu');
+const codeButton = document.getElementById('code_button');
+const codeMenu = document.getElementById('code_menu');
+const robotiqueButton = document.getElementById('robotique_button');
+const robotiqueMenu = document.getElementById('robotique_menu');
+const artButton = document.getElementById('art_button');
+const artMenu = document.getElementById('art_menu');
+const plusButton = document.getElementById('plus_button');
+const plusMenu = document.getElementById('plus_menu');
+
 
 preambuleUpButton.addEventListener('click', function() { goToPreambule() });
 
@@ -76,3 +82,87 @@ function goToAutre() {
     codeCursor.style.marginLeft = "clamp(49vh,490px,49vw)"
     codeCursor.style.width = "clamp(9vh,90px,9vw)"
 }
+
+window.addEventListener('click', event => {
+    if ( !(acceuilMenu.contains(event.target) || 
+        acceuilButton.contains(event.target) ||
+        codeMenu.contains(event.target) || 
+        codeButton.contains(event.target) ||
+        robotiqueMenu.contains(event.target) || 
+        robotiqueButton.contains(event.target) ||
+        artMenu.contains(event.target) || 
+        artButton.contains(event.target) ||
+        plusMenu.contains(event.target)||
+        plusButton.contains(event.target))) {
+            plusMenu.style.display ="none";
+            acceuilMenu.style.display = "none";
+            codeMenu.style.display = "none";
+            robotiqueMenu.style.display = "none";
+            artMenu.style.display = "none";
+    }
+})
+
+acceuilButton.addEventListener('click', function() {
+    if (acceuilMenu.style.display == "block") {
+        acceuilMenu.style.display = "none";
+    } else {
+        acceuilMenu.style.display = "block";
+
+        codeMenu.style.display = "none";
+        robotiqueMenu.style.display = "none";
+        artMenu.style.display = "none";
+        plusMenu.style.display = "none";
+    }
+}) 
+
+codeButton.addEventListener('click', function() {
+    if (codeMenu.style.display == "block") {
+        codeMenu.style.display = "none";
+    } else {
+        codeMenu.style.display = "block";
+
+        acceuilMenu.style.display = "none";
+        robotiqueMenu.style.display = "none";
+        artMenu.style.display = "none";
+        plusMenu.style.display = "none";
+    }
+}) 
+
+robotiqueButton.addEventListener('click', function() {
+    if (robotiqueMenu.style.display == "block") {
+        robotiqueMenu.style.display = "none";
+    } else {
+        robotiqueMenu.style.display = "block";
+
+        acceuilMenu.style.display = "none";
+        codeMenu.style.display = "none";
+        artMenu.style.display = "none";
+        plusMenu.style.display = "none";
+    }
+}) 
+
+artButton.addEventListener('click', function() {
+    if (artMenu.style.display == "block") {
+        artMenu.style.display = "none";
+    } else {
+        artMenu.style.display = "block";
+
+        acceuilMenu.style.display = "none";
+        codeMenu.style.display = "none";
+        robotiqueMenu.style.display = "none";
+        plusMenu.style.display = "none";
+    }
+}) 
+
+plusButton.addEventListener('click', function() {
+    if (plusMenu.style.display == "block") {
+        plusMenu.style.display = "none";
+    } else {
+        plusMenu.style.display = "block";
+
+        acceuilMenu.style.display = "none";
+        codeMenu.style.display = "none";
+        robotiqueMenu.style.display = "none";
+        artMenu.style.display = "none";
+    }
+}) 
